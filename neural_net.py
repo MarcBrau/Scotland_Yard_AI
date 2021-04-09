ef40853c98c85182928720beb3fc02d120091bf6
MarcBrau <50847360+MarcBrau@users.noreply.github.com>
2021-04-09 21:46:16 +0200

    Adapted neural net initialization to differentiate between mister x and detectives

--- neural_net.py
+++ neural_net.py
@@ -10,7 +10,6 @@ sys.path.append('../../')
 from utils import average_meter
 from NeuralNet import NeuralNet
 
-
 from scotland_yard_nnet import ScotlandYardNNet as synnet
 
 args = dict({
@@ -23,75 +22,50 @@ args = dict({
 
 
 class NNetWrapper(NeuralNet):
-    def __init__(self, game):
-        self.nnet = synnet(game, args)
+    def __init__(self, game, is_mister_x=False):
+        self.nnet = synnet(game, args, is_mister_x)
 
     def train(self, examples):
         """
         examples: list of examples, each example is of form (board, pi, v)
         """
-
-        for epoch in range(args["epochs"]):
-            print('EPOCH ::: ' + str(epoch + 1))
-            pi_losses = average_meter()
-            v_losses = average_meter()
-            batch_count = int(len(examples) / args["batch_size"])
-
-            # self.sess.run(tf.local_variables_initializer())
-            t = tqdm(range(batch_count), desc='Training Net')
-            for _ in t:
-                sample_ids = np.random.randint(len(examples), size=args["batch_size"])
-                boards, pis, vs = list(zip(*[examples[i] for i in sample_ids]))
-
-                # predict and compute gradient and do SGD step
-                train_history = self.nnet.model.fit()
-                input_dict = {self.nnet.input_boards: boards, self.nnet.target_pis: pis, self.nnet.target_vs: vs,
-                              self.nnet.dropout: args["dropout"], self.nnet.isTraining: True}
-
-                # record loss
+        input_boards, target_pis, target_vs = list(zip(*examples))
+        input_boards = np.asarray(input_boards)
+        target_pis = np.asarray(target_pis)
+        target_vs = np.asarray(target_vs)
+        self.nnet.model.fit(x=input_boards, y=[target_pis, target_vs], batch_size=args["batch_size"],
+                            epochs=args["epochs"])
 
 
-                self.sess.run(self.nnet.train_step, feed_dict=input_dict)
-                pi_loss, v_loss = self.sess.run([self.nnet.loss_pi, self.nnet.loss_v], feed_dict=input_dict)
-                pi_losses.update(pi_loss, len(boards))
-                v_losses.update(v_loss, len(boards))
-                t.set_postfix(Loss_pi=pi_losses, Loss_v=v_losses)
+def predict(self, board):
+    """
+    board: np array with board
+    """
+    # timing
+    start = time.time()
 
-    def predict(self, board):
-        """
-        board: np array with board
-        """
-        # timing
-        start = time.time()
+    # preparing input
+    board = board[np.newaxis, :, :]
 
-        # preparing input
-        board = board[np.newaxis, :, :]
+    # run
+    pi, v = self.nnet.model.predict(board)
 
-        # run
-        prob, v = self.nnet.predict()
-        prob, v = self.sess.run([self.nnet.prob, self.nnet.v],
-                                feed_dict={self.nnet.input_boards: board, self.nnet.dropout: 0,
-                                           self.nnet.isTraining: False})
+    # print('PREDICTION TIME TAKEN : {0:03f}'.format(time.time()-start))
+    return pi[0], v[0]
 
-        # print('PREDICTION TIME TAKEN : {0:03f}'.format(time.time()-start))
-        return prob[0], v[0]
 
-    def save_checkpoint(self, folder='checkpoint', filename='checkpoint.pth.tar'):
-        filepath = os.path.join(folder, filename)
-        if not os.path.exists(folder):
-            print("Checkpoint Directory does not exist! Making directory {}".format(folder))
-            os.mkdir(folder)
-        else:
-            print("Checkpoint Directory exists! ")
-        if self.saver == None:
-            self.saver = tf.train.Saver(self.nnet.graph.get_collection('variables'))
-        with self.nnet.graph.as_default():
-            self.saver.save(self.sess, filepath)
+def save_checkpoint(self, folder='checkpoint', filename='checkpoint.pth.tar'):
+    filepath = os.path.join(folder, filename)
+    if not os.path.exists(folder):
+        print("Checkpoint Directory does not exist! Making directory {}".format(folder))
+        os.mkdir(folder)
+    else:
+        print("Checkpoint Directory exists! ")
+    self.nnet.model.save_weights(filepath)
 
-    def load_checkpoint(self, folder='checkpoint', filename='checkpoint.pth.tar'):
-        filepath = os.path.join(folder, filename)
-        if not os.path.exists(filepath + '.meta'):
-            raise ("No model in path {}".format(filepath))
-        with self.nnet.graph.as_default():
-            self.saver = tf.train.Saver()
-            self.saver.restore(self.sess, filepath)
+
+def load_checkpoint(self, folder='checkpoint', filename='checkpoint.pth.tar'):
+    filepath = os.path.join(folder, filename)
+    if not os.path.exists(filepath):
+        raise ("No model in path {}".format(filepath))
+    self.nnet.model.load_weights(filepath)
